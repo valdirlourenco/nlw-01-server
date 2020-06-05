6e347dca5e91a584779d009305b4c619e8262154
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-06-04 23:22:03 -0300

    transaction in insert points

--- src/routes.ts
+++ src/routes.ts
@@ -29,9 +29,9 @@ routes.post('/points', async (request, response) => {
         items
     } = request.body;
 
-    console.log(items);
+    const trx = await knex.transaction();
 
-    const ids = await knex('points').insert({
+    const insertedIds = await trx('points').insert({
         image: 'image-fake',
         name,
         email,
@@ -42,14 +42,16 @@ routes.post('/points', async (request, response) => {
         uf
     });
 
+    const point_id = insertedIds[0];
+
     const pointItems = items.map((item_id: number) => {
         return {
             item_id,
-            point_id: ids[0],
+            point_id,
         };
     });
 
-    await knex('point_items').insert(pointItems);
+    await trx('point_items').insert(pointItems);
 
     return response.json({ success: true });
 });
